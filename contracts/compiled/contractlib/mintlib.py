712c7aeeaf306a29f149cbe7ac62c7fcef4e5ba6
Guy Garcia <guysebastiangarcia@gmail.com>
2021-07-24 22:37:47 -0400

    Added full mint contract functionality

--- contracts/compiled/contractlib/mintlib.py
+++ contracts/compiled/contractlib/mintlib.py
@@ -12,6 +12,33 @@ class Mint(Contract):
              "oracle_contract": "none", "oracle_contract_code_hash": "none"})
         super().__init__(contract, initMsg, label, admin, uploader, gas, backend)
 
+    def update_config(self, owner=None, silk_contract=None, silk_contract_code_hash=None,
+                      oracle_contract=None, oracle_contract_code_hash=None):
+        """
+        Updates the minting contract's config
+        :param owner: New admin
+        :param silk_contract: New silk contract address
+        :param silk_contract_code_hash: New silk contract hash
+        :param oracle_contract: New oracle contract address
+        :param oracle_contract_code_hash: New oracle contract hash
+        :return: Result
+        """
+        raw_msg = {"update_config": {}}
+        if owner is not None:
+            raw_msg["update_config"]["owner"] = owner
+        if silk_contract is not None:
+            raw_msg["update_config"]["silk_contract"] = silk_contract
+        if silk_contract_code_hash is not None:
+            raw_msg["update_config"]["silk_contract_code_hash"] = silk_contract_code_hash
+        if oracle_contract is not None:
+            raw_msg["update_config"]["oracle_contract"] = oracle_contract
+        if oracle_contract is not None:
+            raw_msg["update_config"]["oracle_contract"] = oracle_contract
+
+        msg = json.dumps(raw_msg)
+
+        return secretlib.execute_contract(self.address, msg, self.admin, self.backend)
+
     def register_asset(self, snip20):
         """
         Registers a SNIP20 asset
@@ -23,11 +50,43 @@ class Mint(Contract):
 
         return secretlib.execute_contract(self.address, msg, self.admin, self.backend)
 
+    def update_asset(self, old_snip20, snip20):
+        """
+        Updates a SNIP20 asset's info
+        :param old_snip20: The registered snip20
+        :param snip20: New snip20 to replace with
+        :return: Result
+        """
+        msg = json.dumps(
+            {"update_asset": {"asset": old_snip20.address, "contract": snip20.address, "code_hash": snip20.hash}})
+
+        return secretlib.execute_contract(self.address, msg, self.admin, self.backend)
+
+    def get_supported_assets(self):
+        """
+        Get all supported asset addressed
+        :return: Supported assets info
+        """
+        msg = json.dumps(
+            {"get_supported_assets": {}})
+
+        return secretlib.query_contract(self.address, msg)
+
+    def get_config(self):
+        """
+        Get the contracts config information
+        :return: Contract config info
+        """
+        msg = json.dumps(
+            {"get_config": {}})
+
+        return secretlib.query_contract(self.address, msg)
+
     def get_asset(self, snip20):
         """
         Returns that assets info
         :param snip20: SNIP20 object to query
-        :return:
+        :return: Asset info
         """
         msg = json.dumps(
             {"get_asset": {"contract": snip20.address}})
