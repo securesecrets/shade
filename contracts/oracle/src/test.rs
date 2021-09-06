5f0b9b06865dc65e24ba4796132f6deae04feab5
drpresident <drpresident@pop-os.localdomain>
2021-09-06 17:11:24 -0500

    added a few more tests for oracle calcs

--- contracts/oracle/src/test.rs
+++ contracts/oracle/src/test.rs
@@ -19,9 +19,25 @@ mod tests {
 
     normalize_price_tests! {
         normalize_0: (
-            Uint128(1413500852332497), 
-            18u8, 
-            Uint128(1413500852332497)
+            Uint128(1_413_500_852_332_497),
+            18u8,
+            Uint128(1_413_500_852_332_497)
+        ),
+        normalize_1: (
+            // amount of TKN received for 1 sSCRT
+            Uint128(1_000_000),
+            // TKN 6 decimals
+            6u8,
+            // price * 10^18
+            Uint128(1_000_000_000_000_000_000)
+        ),
+        normalize_1: (
+            // amount of TKN received for 1 sSCRT
+            Uint128(1_000_000),
+            // TKN 6 decimals
+            6u8,
+            // price * 10^18
+            Uint128(1_000_000_000_000_000_000)
         ),
     }
 
@@ -39,7 +55,7 @@ mod tests {
 
     translate_price_tests! {
         translate_0: (
-            //SCRT/USD price
+            // 1.62 USD per SCRT
             Uint128(    1_622_110_000_000_000_000), 
             // 1 sSCRT -> sETH
             Uint128(        1_413_500_852_332_497),
@@ -47,12 +63,28 @@ mod tests {
             Uint128(1_147_583_319_333_175_746_166),
         ),
         translate_1: (
-            //SCRT/USD price
+            // 1.62 USD per SCRT
             Uint128(    1_622_110_000_000_000_000), 
-            // SCRT/ETH
+            // .000425 ETH per sSCRT
             Uint128(          425_600_000_000_000),
-            // sETH/USD price
+            // 3811.34 ETH per USD
             Uint128(3_811_348_684_210_526_315_789),
+        ),
+        translate_2: (
+            // 1 USD per scrt
+            Uint128( 1_000_000_000_000_000_000), 
+            // 1 sscrt for .1 SHD
+            Uint128(   100_000_000_000_000_000), 
+            // 10 SHD per USD
+            Uint128(10_000_000_000_000_000_000), 
+        ),
+        translate_3: (
+            // 1 USD per scrt
+            Uint128( 1_000_000_000_000_000_000), 
+            // 1 sscrt for .02 SHD
+            Uint128(    20_000_000_000_000_000), 
+            // 50 SHD per USD
+            Uint128(50_000_000_000_000_000_000), 
         ),
     }
 }
