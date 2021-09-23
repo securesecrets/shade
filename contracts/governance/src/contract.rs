8f96468d24105004fd1617716310c53698fd4005
Guy <guy.garcia@protonmail.com>
2021-09-23 03:01:05 -0400

    added governance contract

---
+++ contracts/governance/src/contract.rs
@@ -0,0 +1,144 @@
+use cosmwasm_std::{to_binary, Api, Binary, Env, Extern, HandleResponse, InitResponse, Querier, StdResult, Storage, Uint128};
+use shade_protocol::{
+    governance::{
+        InitMsg, HandleMsg,
+        QueryMsg, Config,
+    },
+};
+use crate::{
+    state::{config_w, total_proposals_w},
+    handle,
+    query
+};
+use shade_protocol::asset::Contract;
+use crate::state::{admin_commands_list_w, supported_contracts_list_w};
+
+pub fn init<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    env: Env,
+    msg: InitMsg,
+) -> StdResult<InitResponse> {
+
+    let state = Config {
+        admin: match msg.admin {
+            None => { env.message.sender.clone() }
+            Some(admin) => { admin }
+        },
+        proposal_deadline: msg.proposal_deadline,
+        minimum_votes: msg.minimum_votes
+    };
+
+    config_w(&mut deps.storage).save(&state)?;
+
+    // Initialize total proposal counter
+    total_proposals_w(&mut deps.storage).save(&Uint128(0))?;
+
+    // Initialize lists
+    admin_commands_list_w(&mut deps.storage).save(&vec![])?;
+    supported_contracts_list_w(&mut deps.storage).save(&vec![])?;
+
+    Ok(InitResponse {
+        messages: vec![],
+        log: vec![]
+    })
+}
+
+pub fn handle<S: Storage, A: Api, Q: Querier>(
+    deps: &mut Extern<S, A, Q>,
+    env: Env,
+    msg: HandleMsg,
+) -> StdResult<HandleResponse> {
+    match msg {
+        /// Proposals
+        HandleMsg::CreateProposal { target_contract, proposal, description
+        } => handle::try_create_proposal(deps, &env, target_contract,
+                                         Binary::from(proposal.as_bytes()), description),
+
+        // Config proposals
+        HandleMsg::RequestUpdateConfig { admin, proposal_deadline,
+            minimum_votes, description
+        } => handle::try_request_update_config(deps, &env, admin, proposal_deadline,
+                                               minimum_votes, description),
+
+        // Supported contract proposals
+        HandleMsg::RequestAddSupportedContract { name, contract, description
+        } => handle::try_request_add_supported_contract(deps, &env, name, contract, description),
+
+        HandleMsg::RequestRemoveSupportedContract { name, description
+        } => handle::try_request_remove_supported_contract(deps, &env, name, description),
+
+        HandleMsg::RequestUpdateSupportedContract { name, contract, description
+        } => handle::try_request_update_supported_contract(deps, &env, name, contract, description),
+
+        // Admin command proposal
+        HandleMsg::RequestAddAdminCommand {name, proposal, description
+        } => handle::try_request_add_admin_command(deps, &env, name, proposal, description),
+
+        HandleMsg::RequestRemoveAdminCommand {name, description
+        } => handle::try_request_remove_admin_command(deps, &env, name, description),
+
+        HandleMsg::RequestUpdateAdminCommand {name, proposal, description
+        } => handle::try_request_update_admin_command(deps, &env, name, proposal, description),
+
+        /// Self interactions
+        // Config
+        HandleMsg::UpdateConfig { admin, proposal_deadline,
+            minimum_votes } =>
+            handle::try_update_config(deps, &env, admin, proposal_deadline, minimum_votes),
+
+        // Supported contract
+        HandleMsg::AddSupportedContract { name, contract
+        } => handle::try_add_supported_contract(deps, &env, name, contract),
+
+        HandleMsg::RemoveSupportedContract { name
+        } => handle::try_remove_supported_contract(deps, &env, name),
+
+        HandleMsg::UpdateSupportedContract { name, contract
+        } => handle::try_update_supported_contract(deps, &env, name, contract),
+
+        // Admin command
+        HandleMsg::AddAdminCommand { name, proposal
+        } => handle::try_add_admin_command(deps, &env, name, proposal),
+
+        HandleMsg::RemoveAdminCommand { name
+        } => handle::try_remove_admin_command(deps, &env, name),
+
+        HandleMsg::UpdateAdminCommand { name, proposal
+        } => handle::try_update_admin_command(deps, &env, name, proposal),
+
+        /// User interaction
+        HandleMsg::Vote { proposal_id, option
+        } => handle::try_vote(deps, &env, proposal_id, option),
+
+        HandleMsg::TriggerProposal { proposal_id
+        } => handle::try_trigger_proposal(deps, &env, proposal_id),
+
+        /// Admin interactions
+        HandleMsg::TriggerAdminCommand { target, command,
+            variables, description
+        } => handle::try_trigger_admin_command(deps, &env, target, command, variables, description),
+    }
+}
+
+pub fn query<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    msg: QueryMsg,
+) -> StdResult<Binary> {
+    match msg {
+        QueryMsg::GetProposals { total, start
+        } => to_binary(&query::proposals(deps, total, start)?),
+
+        QueryMsg::GetProposal { proposal_id } => to_binary(
+            &query::proposal(deps, proposal_id)?),
+
+        QueryMsg::GetSupportedContracts { } => to_binary(&query::supported_contracts(deps)?),
+
+        QueryMsg::GetSupportedContract { name } => to_binary(
+            &query::supported_contract(deps, name)?),
+
+        QueryMsg::GetAdminCommands {} => to_binary(&query::admin_commands(deps)?),
+
+        QueryMsg::GetAdminCommand { name
+        } => to_binary(&query::admin_command(deps, name)?),
+    }
+}
