e34b9f895b575a15e0ed27c490aacf05133cc44a
Guy <guy.garcia@protonmail.com>
2021-10-19 02:52:33 -0400

    expanded tests

--- packages/network_integration/src/contract_helpers/stake.rs
+++ packages/network_integration/src/contract_helpers/stake.rs
@@ -4,10 +4,11 @@ use shade_protocol::{staking, snip20, asset::Contract};
 use crate::{utils::{print_header, print_contract, print_epoch_info, print_vec,
                     STORE_GAS, GAS, VIEW_KEY, ACCOUNT_KEY},
             contract_helpers::governance::{init_contract, get_contract, add_contract,
-                                           create_proposal, trigger_latest_proposal}};
+                                           create_and_trigger_proposal, trigger_latest_proposal}};
 use secretcli::{cli_types::NetContract,
                 secretcli::{query_contract, test_contract_handle, test_inst_init}};
 use crate::contract_helpers::minter::get_balance;
+use std::{thread, time};
 
 pub fn setup_staker(governance: &NetContract, shade: &Contract, staking_account: String) -> Result<NetContract> {
     let staker = init_contract(&governance, "staking".to_string(),
@@ -16,7 +17,7 @@ pub fn setup_staker(governance: &NetContract, shade: &Contract, staking_account:
                                    admin: Some(Contract{
                                        address: HumanAddr::from(governance.address.clone()),
                                        code_hash: governance.code_hash.clone() }),
-                                   unbond_time: 0,
+                                   unbond_time: 180,
                                    staked_token: Contract {
                                        address: shade.address.clone(),
                                        code_hash: shade.code_hash.clone()
@@ -34,11 +35,13 @@ pub fn setup_staker(governance: &NetContract, shade: &Contract, staking_account:
 
     print_header("Testing staking delegation");
 
-    /// Query current balance
+    // Query current balance
     let original_balance = get_balance(&shade_net, staking_account.clone());
-    let stake_amount = Uint128(100);
+    let stake_amount = Uint128(7000000);
+    let unbond_amount = Uint128(2000000);
+    let balance_after_stake = (original_balance - stake_amount).unwrap();
 
-    /// Stake some Shade on it
+    // Stake some Shade on it
     {
         let msg = snip20::HandleMsg::Send {
             recipient: HumanAddr::from(staker.address.clone()),
@@ -52,29 +55,27 @@ pub fn setup_staker(governance: &NetContract, shade: &Contract, staking_account:
                              Some("test"), None)?;
     }
 
-    /// Check total stake
+    // Check total stake
     assert_eq!(get_total_staked(&staker), stake_amount);
 
-    /// Query total Shade now
-    {
-        let expected_total_shade = (original_balance - stake_amount).unwrap();
+    // Query total Shade now
+    assert_eq!(balance_after_stake, get_balance(&shade_net,
+                                                 staking_account.clone()));
 
-        assert_eq!(expected_total_shade, get_balance(&shade_net,
-                                                     staking_account.clone()));
-    }
-
-    /// User unbonds
+    print_header("Testing unbonding request");
+    // User unbonds
     {
-        let msg = staking::HandleMsg::Unbond { amount: stake_amount };
+        let msg = staking::HandleMsg::Unbond { amount: unbond_amount };
 
         test_contract_handle(&msg, &staker, ACCOUNT_KEY, Some(GAS),
                              Some("test"), None)?;
     }
 
-    /// Check if unstaking
-    assert_eq!(get_total_staked(&staker), Uint128(0));
+    // Check if unstaking
+    assert_eq!(get_total_staked(&staker), (stake_amount - unbond_amount).unwrap());
 
-    /// User triggers
+    print_header("Testing unbonding time");
+    // User triggers but receives nothing
     {
         let msg = staking::HandleMsg::TriggerUnbonds {};
 
@@ -82,11 +83,27 @@ pub fn setup_staker(governance: &NetContract, shade: &Contract, staking_account:
                              Some("test"), None)?;
     }
 
-    /// Query total Shade now
+    // Query total Shade now
     {
-        assert_eq!(original_balance, get_balance(&shade_net,
+        assert_eq!(balance_after_stake, get_balance(&shade_net,
                                                      staking_account.clone()));
     }
+
+    // Wait unbonding time
+    thread::sleep(time::Duration::from_secs(180));
+
+    print_header("Testing unbonding asset release");
+    // User triggers and receives something
+    {
+        let msg = staking::HandleMsg::TriggerUnbonds {};
+
+        test_contract_handle(&msg, &staker, ACCOUNT_KEY, Some(GAS),
+                             Some("test"), None)?;
+    }
+
+    // Query total Shade now
+    assert_eq!((balance_after_stake + unbond_amount), get_balance(&shade_net,
+                                             staking_account.clone()));
 
     Ok(staker)
 }
