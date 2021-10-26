145d9129a595094f3202423afec85ac853373998
Guy <guy.garcia@protonmail.com>
2021-10-26 00:51:04 -0400

    added constants for contract file names

--- packages/network_integration/src/utils.rs
+++ packages/network_integration/src/utils.rs
@@ -8,6 +8,17 @@ use serde::Serialize;
 use secretcli::{cli_types::NetContract,
                 secretcli::{query_contract, test_contract_handle, test_inst_init}};
 
+// Smart contracts
+pub const SNIP20_FILE: &str = "../../compiled/snip20.wasm.gz";
+pub const AIRDROP_FILE: &str = "../../compiled/airdrop.wasm.gz";
+pub const GOVERNANCE_FILE: &str = "../../compiled/governance.wasm.gz";
+pub const MOCK_BAND_FILE: &str = "../../compiled/mock_band.wasm.gz";
+pub const ORACLE_FILE: &str = "../../compiled/oracle.wasm.gz";
+pub const INITIALIZER_FILE: &str = "../../compiled/initializer.wasm.gz";
+pub const MICRO_MINT_FILE: &str = "../../compiled/micro_mint.wasm.gz";
+pub const STAKING_FILE: &str = "../../compiled/staking.wasm.gz";
+
+
 pub const STORE_GAS: &str = "10000000";
 pub const GAS: &str = "800000";
 pub const VIEW_KEY: &str = "password";
