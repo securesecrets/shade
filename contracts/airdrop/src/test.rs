ba60746377c13a5eff60930f5142785fe0aec4df
Guy Garcia <guysebastiangarcia@gmail.com>
2021-12-20 01:40:58 -0500

    testing inverse normalized function

--- contracts/airdrop/src/test.rs
+++ contracts/airdrop/src/test.rs
@@ -6,13 +6,12 @@ pub mod tests {
     use shade_protocol::airdrop::InitMsg;
     use shade_protocol::asset::Contract;
     use crate::contract::init;
-    use crate::handle::calculate_decay_factor;
-
+    use crate::handle::inverse_normalizer;
 
     #[test]
     fn decay_factor() {
-        assert_eq!(Uint128(50), Uint128(100) * calculate_decay_factor(100, 200, 300));
+        assert_eq!(Uint128(50), Uint128(100) * inverse_normalizer(100, 200, 300));
 
-        assert_eq!(Uint128(25), Uint128(100) * calculate_decay_factor(0, 75, 100));
+        assert_eq!(Uint128(25), Uint128(100) * inverse_normalizer(0, 75, 100));
     }
 }
