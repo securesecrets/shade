f7baec840f817d17abb3b779420a11fe14cf8829
Jack Swenson <theicecreamman22@gmail.com>
2022-02-03 14:24:44 -0600

    Treasury allowance (#173)
    * roughed out the allowance interface
    
    * Updates, using chrono utc::now is busted
    
    * Monthly allowance working
    
    * cleaned-up datetime code a little
    
    * fixed merge issue
    
    * updated treasury-synthesis test & added asset list query to treasury
    
    * added one-time allowance & sscrt to config
    
    * added one-time allowance check
    
    * Update contracts/treasury/src/contract.rs
    
    Co-authored-by: Guy S Garcia <34169809+FloppyDisck@users.noreply.github.com>
    
    * makefile adjustments
    
    * small fix to makefile
    
    Co-authored-by: DrPresident <jacksonswenson22@gmail.com>
    Co-authored-by: Guy S Garcia <34169809+FloppyDisck@users.noreply.github.com>

--- contracts/treasury/src/query.rs
+++ contracts/treasury/src/query.rs
@@ -1,8 +1,11 @@
 use cosmwasm_std::{Api, Extern, HumanAddr, Querier, StdError, StdResult, Storage};
-use secret_toolkit::utils::Query;
+use secret_toolkit::{
+    utils::Query,
+    snip20::allowance_query,
+};
 use shade_protocol::{snip20, treasury};
 
-use crate::state::{allocations_r, assets_r, config_r, self_address_r, viewing_key_r};
+use crate::state::{allocations_r, asset_list_r, assets_r, config_r, self_address_r, viewing_key_r, last_allowance_refresh_r};
 
 pub fn config<S: Storage, A: Api, Q: Querier>(
     deps: &Extern<S, A, Q>,
@@ -43,6 +46,49 @@ pub fn balance<S: Storage, A: Api, Q: Querier>(
     }
 }
 
+pub fn allowances<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+    asset: &HumanAddr,
+    spender: &HumanAddr,
+) -> StdResult<treasury::QueryAnswer> {
+
+    let self_address = self_address_r(&deps.storage).load()?;
+    let key = viewing_key_r(&deps.storage).load()?;
+
+    if let Some(full_asset) = assets_r(&deps.storage).may_load(asset.to_string().as_bytes())? {
+
+        let cur_allowance = allowance_query(
+            &deps.querier,
+            self_address,
+            spender.clone(),
+            key,
+            1,
+            full_asset.contract.code_hash.clone(),
+            full_asset.contract.address.clone()
+        )?;
+
+        return Ok(treasury::QueryAnswer::Allowances {
+            allowances: vec![
+                treasury::AllowanceData {
+                    spender: spender.clone(),
+                    amount: cur_allowance.allowance,
+                }
+            ]
+        });
+    }
+
+    Err(StdError::generic_err(format!("Unknown Asset: {}", asset)))
+
+}
+
+pub fn assets<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+) -> StdResult<treasury::QueryAnswer> {
+    Ok(treasury::QueryAnswer::Assets {
+        assets: asset_list_r(&deps.storage).load()?,
+    })
+}
+
 pub fn allocations<S: Storage, A: Api, Q: Querier>(
     deps: &Extern<S, A, Q>,
     asset: HumanAddr,
@@ -54,6 +100,15 @@ pub fn allocations<S: Storage, A: Api, Q: Querier>(
             }
             Some(a) => a,
         },
+    })
+}
+
+pub fn last_allowance_refresh<S: Storage, A: Api, Q: Querier>(
+    deps: &Extern<S, A, Q>,
+) -> StdResult<treasury::QueryAnswer> {
+
+    Ok(treasury::QueryAnswer::Allowances {
+        allowances: vec![],
     })
 }
 
--- packages/shade_protocol/src/snip20.rs
+++ packages/shade_protocol/src/snip20.rs
@@ -2,7 +2,7 @@ use crate::utils::asset::Contract;
 use cosmwasm_std::{Binary, HumanAddr, Querier, StdResult, Uint128};
 use schemars::JsonSchema;
 use secret_toolkit::{
-    snip20::{token_info_query, TokenInfo},
+    snip20::{token_info_query, TokenInfo, Allowance},
     utils::{HandleCallback, InitCallback, Query},
 };
 #[cfg(test)]
@@ -164,6 +164,16 @@ pub enum HandleMsg {
         minters: Vec<HumanAddr>,
         padding: Option<String>,
     },
+    IncreaseAllowance {
+        owner: HumanAddr,
+        spender: HumanAddr,
+        amount: Uint128,
+    },
+    DecreaseAllowance {
+        owner: HumanAddr,
+        spender: HumanAddr,
+        amount: Uint128,
+    },
 }
 
 impl HandleCallback for HandleMsg {
@@ -216,10 +226,13 @@ pub enum QueryAnswer {
         denom: String,
     },
     Allowance {
+        allowance: Allowance,
+        /*
         spender: HumanAddr,
         owner: HumanAddr,
         allowance: Uint128,
         expiration: Option<u64>,
+        */
     },
     Balance {
         amount: Uint128,
