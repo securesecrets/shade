355457cc9288527aef87ad06941a957c35e2d8df
DrPresident <theicecreamman22@gmail.com>
2022-02-04 12:28:57 -0600

    not much

--- packages/shade_protocol/src/sienna.rs
+++ packages/shade_protocol/src/sienna.rs
@@ -21,8 +21,8 @@ pub struct AssetInfo {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub struct Asset {
-    pub amount: Uint128,
     pub token: AssetInfo,
+    pub amount: Uint128,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
