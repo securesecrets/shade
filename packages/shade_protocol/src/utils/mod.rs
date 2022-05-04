bdc7f476aa5c55486f278280cc9c41d5449ffdb1
DrPresident <theicecreamman22@gmail.com>
2022-05-04 14:28:54 -0500

    features

--- packages/shade_protocol/src/utils/mod.rs
+++ packages/shade_protocol/src/utils/mod.rs
@@ -13,7 +13,10 @@ pub mod flexible_msg;
 pub mod generic_response;
 
 pub mod storage;
+
+#[cfg(feature = "utils")]
 pub mod cycle;
+#[cfg(feature = "utils")]
 pub mod wrap;
 
 #[cfg(feature = "math")]
