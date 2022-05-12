7490ce35790715019a8531709d5861c55f1ff216
DrPresident <theicecreamman22@gmail.com>
2022-05-12 18:27:03 -0500

    added dao harnesses

--- packages/contract_harness/src/harness.rs
+++ packages/contract_harness/src/harness.rs
@@ -33,3 +33,30 @@ pub mod mock_band {
     pub struct MockBand;
     harness_macro::implement_harness!(MockBand, mock_band);
 }
+
+#[cfg(feature = "treasury")]
+pub mod treasury {
+    use treasury;
+    use crate::harness_macro;
+
+    pub struct Treasury;
+    harness_macro::implement_harness!(Treasury, treasury);
+}
+
+#[cfg(feature = "treasury_manager")]
+pub mod treasury_manager {
+    use treasury_manager;
+    use crate::harness_macro;
+
+    pub struct TreasuryManager;
+    harness_macro::implement_harness!(TreasuryManager, treasury_manager);
+}
+
+#[cfg(feature = "scrt_staking")]
+pub mod scrt_staking {
+    use scrt_staking;
+    use crate::harness_macro;
+
+    pub struct ScrtStaking;
+    harness_macro::implement_harness!(ScrtStaking, scrt_staking);
+}
