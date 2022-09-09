1bccbf1140ec9cd058afbdad4b7851ce54e37000
SissonJ <sissonjack67@gmail.com>
2022-09-09 10:38:06 -0500

    fixed and tested cycle check fn

--- packages/shade_protocol/src/utils/cycle.rs
+++ packages/shade_protocol/src/utils/cycle.rs
@@ -43,30 +43,54 @@ pub fn utc_now(env: &Env) -> DateTime<Utc> {
 }
 
 pub fn exceeds_cycle(now: &DateTime<Utc>, last_refresh: &DateTime<Utc>, cycle: Cycle) -> bool {
+    if now.timestamp() < last_refresh.timestamp() && cycle != Cycle::Constant {
+        return false;
+    }
     match cycle {
         Cycle::Constant => true,
         Cycle::Once => false,
         Cycle::Seconds { seconds } => {
-            seconds >= Uint128::new((now.timestamp() - last_refresh.timestamp()) as u128)
+            println!(
+                "{} >= {} -> {} - {}",
+                seconds,
+                now.timestamp() - last_refresh.timestamp(),
+                now.timestamp(),
+                last_refresh.timestamp()
+            );
+            seconds <= Uint128::new((now.timestamp() - last_refresh.timestamp()) as u128)
         }
         Cycle::Minutes { minutes } => {
+            println!(
+                "{} >= {} -> {} - {}",
+                minutes,
+                ((now.timestamp() - last_refresh.timestamp()) / 60),
+                now.timestamp(),
+                last_refresh.timestamp()
+            );
             minutes
-                >= Uint128::new(
+                <= Uint128::new(
                     ((now.timestamp() - last_refresh.timestamp()) / 60)
                         .try_into()
                         .unwrap(),
                 )
         }
         Cycle::Hourly { hours } => {
+            println!(
+                "{} >= {} -> {} - {}",
+                hours,
+                ((now.timestamp() - last_refresh.timestamp()) / 60 / 60),
+                now.timestamp(),
+                last_refresh.timestamp()
+            );
             hours
-                >= Uint128::new(
+                <= Uint128::new(
                     ((now.timestamp() - last_refresh.timestamp()) / 60 / 60)
                         .try_into()
                         .unwrap(),
                 )
         }
         Cycle::Daily { days } => {
-            now.num_days_from_ce() - last_refresh.num_days_from_ce() >= days.u128() as i32
+            days.u128() as i32 <= now.num_days_from_ce() - last_refresh.num_days_from_ce()
         }
         Cycle::Monthly { months } => {
             let month_diff: u32;
@@ -77,10 +101,10 @@ pub fn exceeds_cycle(now: &DateTime<Utc>, last_refresh: &DateTime<Utc>, cycle: C
                 month_diff = now.month() - last_refresh.month();
             }
 
-            month_diff >= months.u128() as u32
+            months.u128() as u32 <= month_diff
         }
         Cycle::Yearly { years } => {
-            now.year_ce().1 - last_refresh.year_ce().1 >= years.u128() as u32
+            years.u128() as u32 <= now.year_ce().1 - last_refresh.year_ce().1
         }
     }
 }
@@ -109,30 +133,197 @@ mod test {
     }
 
     exceeds_cycle_tests! {
-        daily_cycle_1day: (
-            "2019-10-12T07:20:50.52Z",
-            "2019-10-13T07:20:50.52Z",
+        constant_cycle: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T23:59:59.59Z",
+            Cycle::Constant,
+            true,
+        ),
+        once_cycle: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T23:59:59.59Z",
+            Cycle::Once,
+            false,
+        ),
+        seconds_cycle_well_under: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T00:00:05.00Z", // 5 sec diff
+            Cycle::Seconds { seconds: Uint128::new(10) },
+            false,
+        ),
+        seconds_cycle_just_short: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T00:00:09.99Z", // 9.99 sec diff
+            Cycle::Seconds { seconds: Uint128::new(10) },
+            false,
+        ),
+        seconds_cycle_exact: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T00:00:10.00Z", // 10 sec diff
+            Cycle::Seconds { seconds: Uint128::new(10) },
+            true,
+        ),
+        seconds_cycle_well_over: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T00:20:00.00Z", // 20 min diff
+            Cycle::Seconds { seconds: Uint128::new(10) },
+            true,
+        ),
+        seconds_cycle_overflow: (
+            "2019-10-12T00:20:00.00Z", // 20 min diff
+            "2019-10-12T00:00:00.00Z",
+            Cycle::Seconds { seconds: Uint128::new(10) },
+            false,
+        ),
+        minutes_cycle_well_under: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T00:00:30.00Z", // 30 sec diff
+            Cycle::Minutes { minutes: Uint128::new(1) },
+            false,
+        ),
+        minutes_cycle_just_short: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T00:00:59.99Z", // 59.99 sec diff
+            Cycle::Minutes { minutes: Uint128::new(1) },
+            false,
+        ),
+        minutes_cycle_exact: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T00:01:00.00Z", // 1 min diff
+            Cycle::Minutes { minutes: Uint128::new(1) },
+            true,
+        ),
+        minutes_cycle_well_over: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T00:20:00.00Z", // 20 min diff
+            Cycle::Minutes { minutes: Uint128::new(1) },
+            true,
+        ),
+        minutes_cycle_overflow: (
+            "2019-10-12T00:20:00.00Z", // 20 min diff
+            "2019-10-12T00:00:00.00Z",
+            Cycle::Minutes { minutes: Uint128::new(1) },
+            false,
+        ),
+        hours_cycle_well_under: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T00:30:00.00Z", // 30 min diff
+            Cycle::Hourly { hours: Uint128::new(1) },
+            false,
+        ),
+        hours_cycle_just_short: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T00:59:59.99Z", // 59 mint 59.99 sec diff
+            Cycle::Hourly { hours: Uint128::new(1) },
+            false,
+        ),
+        hours_cycle_exact: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T01:00:00.00Z", // 1 hour diff
+            Cycle::Hourly { hours: Uint128::new(1) },
+            true,
+        ),
+        hours_cycle_well_over: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T01:30:00.00Z", // 1 hour 30 min diff
+            Cycle::Hourly { hours: Uint128::new(1) },
+            true,
+        ),
+        hours_cycle_overflow: (
+            "2019-10-12T01:30:00.00Z", // 1 hour 30 min diff
+            "2019-10-12T00:00:00.00Z",
+            Cycle::Hourly { hours: Uint128::new(1) },
+            false,
+        ),
+        daily_cycle_well_under: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T23:00:00.00Z", // 23 hour diff
+            Cycle::Daily { days: Uint128::new(1)},
+            false,
+        ),
+        daily_cycle_just_short: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-12T23:59:59.99Z", // 23 hours, 59 min, 59.99 sec diff
+            Cycle::Daily { days: Uint128::new(1) },
+            false,
+        ),
+        daily_cycle_exact: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-13T00:00:00.00Z", // 1 day diff
             Cycle::Daily { days: Uint128::new(1) },
             true,
         ),
-        //TODO Fix
-        /*daily_cycle_1y: (
-            "2019-10-12T07:20:50.51Z",
-            "2020-10-12T07:20:50.51Z",
-            Cycle::Daily { days: Uint128::new(1)},
-            false,
+        daily_cycle_well_over: (
+            "2019-10-12T00:00:00.00Z",
+            "2019-10-13T12:00:00.00Z", // 1 day 12 hr diff
+            Cycle::Daily { days: Uint128::new(1) },
+            true,
         ),
-        daily_cycle_23h: (
-            "2019-10-12T07:20:50.52Z",
-            "2019-10-13T06:20:50.52Z",
-            Cycle::Daily { days: Uint128::new(1)},
-            false,
-        ),
-        daily_cycle_1s_short: (
-            "2019-10-12T07:20:50.51Z",
-            "2019-10-13T07:20:49.51Z",
+        daily_cycle_overflow: (
+            "2019-10-13T12:00:00.00Z", // 1 day 12 hr diff
+            "2019-10-12T00:00:00.00Z",
             Cycle::Daily { days: Uint128::new(1) },
             false,
-        ),*/
+        ),
+        monthly_cycle_well_under: (
+            "2019-10-01T00:00:00.00Z",
+            "2019-10-15T00:00:00.00Z", // 14 day diff
+            Cycle::Monthly { months: Uint128::new(1) },
+            false,
+        ),
+        monthly_cycle_just_short: (
+            "2019-10-01T00:00:00.00Z",
+            "2019-10-31T23:59:59.99Z", // 30 day, 23 hr, 59 min, 59.99 sec diff
+            Cycle::Monthly { months: Uint128::new(1) },
+            false,
+        ),
+        monthly_cycle_exact: (
+            "2019-10-01T00:00:00.00Z",
+            "2019-11-01T00:00:00.00Z", // 1 mo diff
+            Cycle::Monthly { months: Uint128::new(1) },
+            true,
+        ),
+        monthly_cycle_well_over: (
+            "2019-10-01T00:00:00.00Z",
+            "2019-11-15T00:00:00.00Z", // 1 mo, 15 day diff
+            Cycle::Monthly { months: Uint128::new(1) },
+            true,
+        ),
+        monthly_cycle_overflow: (
+            "2019-11-15T00:00:00.00Z", // 1 mo, 15 day diff
+            "2019-10-01T00:00:00.00Z",
+            Cycle::Monthly { months: Uint128::new(1) },
+            false,
+        ),
+        yearly_cycle_well_under: (
+            "2019-01-01T00:00:00.00Z",
+            "2019-10-01T00:00:00.00Z", // 9 mo diff
+            Cycle::Yearly { years: Uint128::new(1) },
+            false,
+        ),
+        yearly_cycle_just_short: (
+            "2019-01-01T00:00:00.00Z",
+            "2019-12-31T23:59:59.99Z", // 11 mo, 31 days, 23 hrs, 59 mins 59.99 sec diff
+            Cycle::Yearly { years: Uint128::new(1) },
+            false,
+        ),
+        yearly_cycle_exact: (
+            "2019-01-01T00:00:00.00Z",
+            "2020-01-01T00:00:00.00Z", // 1 yr diff
+            Cycle::Yearly { years: Uint128::new(1) },
+            true,
+        ),
+        yearly_cycle_well_over: (
+            "2019-01-01T00:00:00.00Z",
+            "2020-06-01T00:00:00.00Z", // 1 yr, 6 mo diff
+            Cycle::Yearly { years: Uint128::new(1) },
+            true,
+        ),
+        yearly_cycle_overflow: (
+            "2020-06-01T00:00:00.00Z", // 1 yr, 6 mo diff
+            "2019-01-01T00:00:00.00Z",
+            Cycle::Yearly { years: Uint128::new(1) },
+            false,
+        ),
     }
 }
