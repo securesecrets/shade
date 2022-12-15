fecdabc63e1c9bed0c545ee4cf3ae67bc0929f32
a-stgeorge <aidanstgeorge1@gmail.com>
2022-12-15 12:14:03 -0600

    Arb optimizition tests

--- contracts/sky_derivatives/src/tests/query.rs
+++ contracts/sky_derivatives/src/tests/query.rs
@@ -29,4 +29,44 @@ fn optimization_math() {
             direction: Some(Direction::Unbond),
         },
     );
+
+    assert_eq!(
+        query::optimization_math(
+            (Float::from(1_070_000_000_000u128), Float::from(1_000_000_000_000u128)),
+            Float::from_float(1.081),
+            Float::from_float(0.9995),
+            Float::from_float(0.998),
+            Float::from_float(0.997),
+            None,
+        ).unwrap(),
+        QueryAnswer::IsProfitable {
+            is_profitable: true,
+            swap_amounts: Some((
+                    Uint128::from(3_602_994_595u32),
+                    Uint128::from(3_345_916_162u32),
+                    Uint128::from(3_615_126_904u32))),
+            direction: Some(Direction::Unbond),
+        },
+    );
+
+    assert_eq!(
+        query::optimization_math(
+            (Float::from(1_087_500_000_000u128), Float::from(1_000_000_000_000u128)),
+            Float::from_float(1.081),
+            Float::from_float(0.9995),
+            Float::from_float(0.998),
+            Float::from_float(0.997),
+            None,
+        ).unwrap(),
+        QueryAnswer::IsProfitable {
+            is_profitable: true,
+            swap_amounts: Some((
+                    Uint128::from(535_451_320u32),
+                    Uint128::from(494_338_961u32),
+                    Uint128::from(535_716_014u32))),
+            direction: Some(Direction::Stake),
+        },
+    );
+
+
 }
