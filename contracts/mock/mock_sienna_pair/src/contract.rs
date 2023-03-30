c0cf5cf94ec1dead7299121726ccff58fad03ec8
a-stgeorge <aidanstgeorge1@gmail.com>
2023-03-30 10:15:48 -0500

    undo debug 1

--- contracts/mock/mock_sienna_pair/src/contract.rs
+++ contracts/mock/mock_sienna_pair/src/contract.rs
@@ -145,8 +145,6 @@ pub fn execute(
             amount,
             ..
         } => {
-            println!("Receive msg recieved!");
-
             let msg = msg.ok_or_else(|| {
                 StdError::generic_err("Receiver callback \"msg\" parameter cannot be empty.")
             })?;
@@ -209,7 +207,6 @@ pub fn execute(
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
         QueryMsg::PairInfo => {
-            println!("Getting info! (print works)");
             let config = Config::load(deps.storage)?;
             let pair_info = PairInfo::load(deps.storage)?;
             let (amount_0, amount_1) = query_pool_amounts(
