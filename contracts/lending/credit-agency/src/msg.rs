eced1f825c4a3621c62d9d3359e817463500e76d
Jakub <jakub@moonbite.space>
2023-12-19 10:46:56 +0100

    Credit agency: Execute messages

--- contracts/lending/credit-agency/src/msg.rs
+++ contracts/lending/credit-agency/src/msg.rs
@@ -28,9 +28,85 @@ pub struct InstantiateMsg {
 }
 
 #[cw_serde]
+pub enum ExecuteMsg {
+    CreateMarket(MarketConfig),
+    /// Tries to perform liquidation on passed account using collateral's denom. The native tokens
+    /// sent along with this message define the debt market.
+    Liquidate {
+        account: String,
+        collateral_denom: Token,
+    },
+    /// Ensures a given account has entered a market. Meant to be called by a specific
+    /// market contract - so the sender of the msg would be the market. The store is treated as a set.
+    EnterMarket {
+        account: String,
+    },
+    /// Exits market if:
+    /// * Sender have no debt in the market
+    /// * Sender have no CTokens in the market, or collateral provided by owned CTokens
+    ///   is not affecting liquidity of sender
+    ExitMarket {
+        /// Address of the `isotonic-market` sender want to exit from.
+        market: String,
+    },
+    /// Repay a loan by using some indicated collateral.
+    /// The collateral is traded on Wynd DEX.
+    RepayWithCollateral {
+        /// The maximum amount of collateral to use
+        max_collateral: Coin,
+        /// How much of the loan is trying to be repaid
+        amount_to_repay: Coin,
+    },
+    /// Handles contract's logics that involves receiving Snip20 tokens.
+    Receive(Snip20ReceiveMsg),
+    /// Sender must be the Governance Contract
+    AdjustMarketId {
+        new_market_id: u64,
+    },
+    /// Sender must be the Governance Contract
+    AdjustTokenId {
+        new_token_id: u64,
+    },
+    /// Sets common_token parameter in configuration and sends AdjustCommonToken
+    /// message to all affiliated markets
+    ///
+    /// Sender must be the Governance Contract
+    AdjustCommonToken {
+        new_common_token: Token,
+    },
+}
+
+#[cw_serde]
 pub enum ReceiveMsg {
     Liquidate {
         account: String,
         collateral_denom: Token,
     },
+}
+
+#[cw_serde]
+pub struct MarketConfig {
+    /// Name used to create the cToken name `Lent ${name}`.
+    /// Forwarded to `isotonic-token`.
+    pub name: String,
+    /// Symbol used to create the cToken `C${symbol}`.
+    /// Forwarded to `isotonic-token`.
+    pub symbol: String,
+    /// Decimals for cToken.
+    /// Forwarded to `isotonic-token`.
+    pub decimals: u8,
+    /// Token for the market token
+    pub market_token: Token,
+    /// An optional cap on total number of tokens deposited into the market
+    pub market_cap: Option<Uint128>,
+    /// Interest rate curve
+    pub interest_rate: Interest,
+    /// Define interest's charged period (in seconds)
+    pub interest_charge_period: u64,
+    /// Ratio of how much tokens can be borrowed for one unit, 0 <= x < 1
+    pub collateral_ratio: Decimal,
+    /// Address of contract to query for price
+    pub price_oracle: String,
+    /// Defines the portion of borrower interest that is converted into reserves (0 <= x <= 1)
+    pub reserve_factor: Decimal,
 }
