7b9c85bbba8069e7e5887015ebf205b29ef7865a
itshaseebsaeed <haseebsyed30000@gmail.com>
2023-12-22 07:10:23 +0500

    lb_factory exec and query responses added

---
+++ contracts/liquidity_book/lb_factory/src/bin/secretcli/example_data.rs
@@ -0,0 +1,187 @@
+use shade_protocol::{
+    c_std::{to_binary, Addr, ContractInfo, Uint128, Uint256},
+    lb_libraries::types::{
+        ContractInstantiationInfo,
+        LBPair,
+        LBPairInformation,
+        StaticFeeParameters,
+    },
+    liquidity_book::lb_pair::{LiquidityParameters, RemoveLiquidity},
+    snip20::Snip20ReceiveMsg,
+    swap::core::{TokenAmount, TokenType},
+    utils::asset::RawContract,
+};
+
+pub const BIN_STEP: u16 = 100u16;
+pub const ACTIVE_ID: u32 = 8_388_608u32;
+
+pub trait VariousAddr {
+    fn owner() -> Self;
+    fn admin() -> Self;
+    fn sender() -> Self;
+    fn recipient() -> Self;
+    fn funds_recipient() -> Self;
+    fn contract() -> Self;
+}
+
+impl VariousAddr for Addr {
+    fn owner() -> Self {
+        Addr::unchecked("secret1...owner")
+    }
+
+    fn admin() -> Self {
+        Addr::unchecked("secret1...admin")
+    }
+
+    fn sender() -> Self {
+        Addr::unchecked("secret1...sender")
+    }
+
+    fn recipient() -> Self {
+        Addr::unchecked("secret1...recipient")
+    }
+
+    fn funds_recipient() -> Self {
+        Addr::unchecked("secret1...fundsrecipient")
+    }
+
+    fn contract() -> Self {
+        Addr::unchecked("secret1...foobar")
+    }
+}
+
+pub trait ExampleData {
+    fn example() -> Self;
+}
+
+impl ExampleData for ContractInstantiationInfo {
+    fn example() -> Self {
+        ContractInstantiationInfo {
+            id: 1u64,
+            code_hash: "0123456789ABCDEF".to_string(),
+        }
+        .clone()
+    }
+}
+
+impl ExampleData for TokenType {
+    fn example() -> Self {
+        TokenType::CustomToken {
+            contract_addr: Addr::contract(),
+            token_code_hash: "0123456789ABCDEF".to_string(),
+        }
+        .clone()
+    }
+}
+
+impl ExampleData for TokenAmount {
+    fn example() -> Self {
+        TokenAmount {
+            token: TokenType::example(),
+            amount: Uint128::from(100u32),
+        }
+    }
+}
+
+impl ExampleData for ContractInfo {
+    fn example() -> Self {
+        ContractInfo {
+            address: Addr::contract(),
+            code_hash: "0123456789ABCDEF".to_string(),
+        }
+        .clone()
+    }
+}
+
+// TODO - why are we using this instead of ContractInfo?
+impl ExampleData for RawContract {
+    fn example() -> Self {
+        RawContract {
+            address: Addr::contract().to_string(),
+            code_hash: "0123456789ABCDEF".to_string(),
+        }
+        .clone()
+    }
+}
+
+impl ExampleData for StaticFeeParameters {
+    fn example() -> Self {
+        StaticFeeParameters {
+            base_factor: 100,
+            filter_period: 100,
+            decay_period: 100,
+            reduction_factor: 100,
+            variable_fee_control: 100,
+            protocol_share: 100,
+            max_volatility_accumulator: 100,
+        }
+    }
+}
+
+impl ExampleData for LiquidityParameters {
+    fn example() -> Self {
+        LiquidityParameters {
+            token_x: TokenType::example(),
+            token_y: TokenType::example(),
+            bin_step: BIN_STEP,
+            amount_x: Uint128::from(110u128),
+            amount_y: Uint128::from(110u128),
+            amount_x_min: Uint128::from(110u128),
+            amount_y_min: Uint128::from(110u128),
+            active_id_desired: ACTIVE_ID,
+            // TODO - write some function that converts a price slippage % to an id_slippage (would
+            // depend on bin_step)
+            id_slippage: 1000u32,
+            // TODO - I think these need to be much larger to hit the proper bin ids corresponding
+            // to the next bin_step price.
+            delta_ids: vec![-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5],
+            distribution_x: vec![10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+            distribution_y: vec![10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+            deadline: 1701283067,
+        }
+    }
+}
+
+impl ExampleData for LBPairInformation {
+    fn example() -> Self {
+        LBPairInformation {
+            bin_step: 100,
+            lb_pair: LBPair {
+                token_x: TokenType::example(),
+                token_y: TokenType::example(),
+                bin_step: 100,
+                contract: ContractInfo::example(),
+            },
+            created_by_owner: true,
+            ignored_for_routing: false,
+        }
+    }
+}
+
+impl ExampleData for RemoveLiquidity {
+    fn example() -> Self {
+        RemoveLiquidity {
+            token_x: TokenType::example(),
+            token_y: TokenType::example(),
+            bin_step: BIN_STEP,
+            amount_x_min: Uint128::from(10u128),
+            amount_y_min: Uint128::from(10u128),
+            ids: vec![ACTIVE_ID],
+            // TODO - understand what "amounts" means. Is that a packed_uint128?
+            amounts: vec![Uint256::from_u128(10u128)],
+            deadline: 1701283067,
+        }
+    }
+}
+
+impl ExampleData for Snip20ReceiveMsg {
+    fn example() -> Self {
+        Snip20ReceiveMsg {
+            sender: Addr::contract().to_string(),
+            from: Addr::sender().to_string(),
+            amount: Uint128::from(100u128),
+            memo: None,
+            msg: Some(to_binary(&"base64 encoded string").unwrap()),
+        }
+    }
+}
---
+++ contracts/liquidity_book/lb_factory/src/bin/secretcli/main.rs
@@ -0,0 +1,323 @@
+mod example_data;
+
+use example_data::{ExampleData, VariousAddr, ACTIVE_ID, BIN_STEP};
+use shade_protocol::{
+    c_std::{Addr, ContractInfo},
+    contract_interfaces::liquidity_book::lb_factory::{ExecuteMsg, InstantiateMsg, QueryMsg},
+    lb_libraries::types::{ContractInstantiationInfo, LBPair, LBPairInformation},
+    liquidity_book::{lb_factory::*, lb_pair::RewardsDistributionAlgorithm},
+    swap::core::TokenType,
+    utils::asset::RawContract,
+};
+use std::{
+    env,
+    fs::File,
+    io::{self, Write},
+    path::Path,
+};
+
+macro_rules! print_instantiate_message {
+    ($file:ident, $($var:ident),+ $(,)?) => {
+        $(
+            writeln!($file,
+                "```sh\nsecretcli tx compute instantiate 1 '{}'\n```",
+                serde_json::to_string_pretty(&$var).unwrap()
+            )?;
+            writeln!($file, "")?;
+        )+
+    };
+}
+
+macro_rules! print_execute_messages {
+    ($file:ident, $($var:ident),+ $(,)?) => {
+        $(
+            writeln!($file,
+                "### {}\n\n```sh\nsecretcli tx compute execute secret1foobar '{}'\n```",
+                stringify!($var),
+                serde_json::to_string_pretty(&$var).unwrap()
+            )?;
+            writeln!($file, "")?;
+        )+
+    };
+}
+
+macro_rules! print_query_messages {
+    ($file:ident, $($var:ident),+ $(,)?) => {
+        $(
+            writeln!($file,
+                "### {}\n\n```sh\nsecretcli query compute query secret1foobar '{}'\n```",
+                stringify!($var),
+                serde_json::to_string_pretty(&$var).unwrap()
+            )?;
+            writeln!($file, "\n")?;
+        )+
+    };
+}
+
+macro_rules! print_query_messages_with_responses {
+      ($file:ident, $(($cmd:ident, $resp:ident)),+ $(,)?) => {
+          $(
+              writeln!($file,
+                  "### {}\n\n```sh\nsecretcli query compute query secret1foobar '{}'\n```\n",
+                  stringify!($cmd),
+                  serde_json::to_string_pretty(&$cmd).unwrap()
+              )?;
+              writeln!($file,
+                  "#### Response\n\n```json\n{}\n```\n",
+                  serde_json::to_string_pretty(&$resp).unwrap()
+              )?;
+          )+
+      };
+  }
+
+fn main() -> io::Result<()> {
+    let crate_root = &env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
+    let package_name = &env::var("CARGO_PKG_NAME").expect("CARGO_PKG_NAME not set");
+    let file_path = Path::new(crate_root).join(format!("{package_name}.md"));
+    let mut file = File::create(file_path)?;
+
+    writeln!(file, "# {package_name}\n")?;
+
+    // -- Instantiate Message
+
+    let instantiate_msg = InstantiateMsg {
+        admin_auth: RawContract::example(),
+        owner: Some(Addr::owner()),
+        fee_recipient: Addr::recipient(),
+        total_reward_bins: 10,
+        rewards_distribution_algorithm: RewardsDistributionAlgorithm::TimeBasedRewards,
+        epoch_staking_index: 1,
+        epoch_staking_duration: 100,
+        expiry_staking_duration: None,
+        recover_staking_funds_receiver: Addr::funds_recipient(),
+    };
+
+    writeln!(file, "## Instantiate Message\n")?;
+    print_instantiate_message!(file, instantiate_msg);
+
+    // -- Execute Messages
+
+    let set_lb_pair_implementation = ExecuteMsg::SetLBPairImplementation {
+        implementation: ContractInstantiationInfo::example(),
+    };
+
+    let set_lb_token_implementation = ExecuteMsg::SetLBTokenImplementation {
+        implementation: ContractInstantiationInfo::example(),
+    };
+
+    let create_lb_pair = ExecuteMsg::CreateLBPair {
+        token_x: TokenType::example(),
+        token_y: TokenType::example(),
+        // note: active id is a function of price
+        active_id: ACTIVE_ID,
+        bin_step: BIN_STEP,
+        viewing_key: "api_key_etc".to_string(),
+        entropy: "shade rocks".to_string(),
+    };
+
+    // TODO - find out what are more reasonable example values
+    let set_pair_preset = ExecuteMsg::SetPairPreset {
+        bin_step: BIN_STEP,
+        base_factor: 100,
+        filter_period: 100,
+        decay_period: 100,
+        reduction_factor: 100,
+        variable_fee_control: 100,
+        protocol_share: 100,
+        max_volatility_accumulator: 100,
+        is_open: true,
+        total_reward_bins: 0,
+    };
+
+    let set_preset_open_state = ExecuteMsg::SetPresetOpenState {
+        bin_step: BIN_STEP,
+        is_open: true,
+    };
+
+    let remove_preset = ExecuteMsg::RemovePreset { bin_step: BIN_STEP };
+
+    let set_fee_parameters_on_pair = ExecuteMsg::SetFeeParametersOnPair {
+        token_x: TokenType::example(),
+        token_y: TokenType::example(),
+        bin_step: BIN_STEP,
+        base_factor: 100,
+        filter_period: 100,
+        decay_period: 100,
+        reduction_factor: 100,
+        variable_fee_control: 100,
+        protocol_share: 100,
+        max_volatility_accumulator: 100,
+    };
+
+    let set_fee_recipient = ExecuteMsg::SetFeeRecipient {
+        fee_recipient: Addr::recipient(),
+    };
+
+    let add_quote_asset = ExecuteMsg::AddQuoteAsset {
+        asset: TokenType::example(),
+    };
+
+    let remove_quote_asset = ExecuteMsg::RemoveQuoteAsset {
+        asset: TokenType::example(),
+    };
+
+    let force_decay = ExecuteMsg::ForceDecay {
+        pair: LBPair {
+            token_x: TokenType::example(),
+            token_y: TokenType::example(),
+            bin_step: BIN_STEP,
+            contract: ContractInfo::example(),
+        },
+    };
+
+    writeln!(file, "## Execute Messages\n")?;
+    print_execute_messages!(
+        file,
+        set_lb_pair_implementation,
+        set_lb_token_implementation,
+        create_lb_pair,
+        set_pair_preset,
+        set_preset_open_state,
+        remove_preset,
+        set_fee_parameters_on_pair,
+        set_fee_recipient,
+        add_quote_asset,
+        remove_quote_asset,
+        force_decay
+    );
+
+    // -- Query Messages
+
+    let get_min_bin_step = QueryMsg::GetMinBinStep {};
+    let get_fee_recipient = QueryMsg::GetFeeRecipient {};
+    let get_lb_pair_implementation = QueryMsg::GetLBPairImplementation {};
+    let get_lb_token_implementation = QueryMsg::GetLBTokenImplementation {};
+    let get_number_of_lb_pairs = QueryMsg::GetNumberOfLBPairs {};
+    let get_lb_pair_at_index = QueryMsg::GetLBPairAtIndex { index: 0 };
+    let get_number_of_quote_assets = QueryMsg::GetNumberOfQuoteAssets {};
+    let get_quote_asset_at_index = QueryMsg::GetQuoteAssetAtIndex { index: 0 };
+    let is_quote_asset = QueryMsg::IsQuoteAsset {
+        token: TokenType::example(),
+    };
+    let get_lb_pair_information = QueryMsg::GetLBPairInformation {
+        token_x: TokenType::example(),
+        token_y: TokenType::example(),
+        bin_step: BIN_STEP,
+    };
+    let get_preset = QueryMsg::GetPreset { bin_step: BIN_STEP };
+    let get_all_bin_steps = QueryMsg::GetAllBinSteps {};
+    let get_open_bin_steps = QueryMsg::GetOpenBinSteps {};
+    let get_all_lb_pairs = QueryMsg::GetAllLBPairs {
+        token_x: TokenType::example(),
+        token_y: TokenType::example(),
+    };
+
+    // responses
+
+    let get_min_bin_step_response = MinBinStepResponse { min_bin_step: 100 };
+    let get_fee_recipient_response = FeeRecipientResponse {
+        fee_recipient: Addr::recipient(),
+    };
+    let get_lb_pair_implementation_response = LBPairImplementationResponse {
+        lb_pair_implementation: ContractInstantiationInfo::example(),
+    };
+    let get_lb_token_implementation_response = LBTokenImplementationResponse {
+        lb_token_implementation: ContractInstantiationInfo::example(),
+    };
+    let get_number_of_lb_pairs_response = NumberOfLBPairsResponse { lb_pair_number: 1 };
+
+    let get_lb_pair_at_index_response = LBPairAtIndexResponse {
+        lb_pair: LBPair {
+            token_x: TokenType::example(),
+            token_y: TokenType::example(),
+            bin_step: 100,
+            contract: ContractInfo::example(),
+        },
+    };
+
+    let get_number_of_quote_assets_response = NumberOfQuoteAssetsResponse {
+        number_of_quote_assets: 10,
+    };
+
+    let get_quote_asset_at_index_response = QuoteAssetAtIndexResponse {
+        asset: TokenType::example(),
+    };
+
+    let is_quote_asset_response = IsQuoteAssetResponse { is_quote: true };
+
+    let get_lb_pair_information_response = LBPairInformationResponse {
+        lb_pair_information: LBPairInformation::example(),
+    };
+
+    let get_preset_response = PresetResponse {
+        base_factor: 100,
+        filter_period: 100,
+        decay_period: 100,
+        reduction_factor: 100,
+        variable_fee_control: 100,
+        protocol_share: 100,
+        max_volatility_accumulator: 100,
+        is_open: false,
+    };
+
+    let get_all_bin_steps_response = AllBinStepsResponse {
+        bin_step_with_preset: vec![20, 50, 100],
+    };
+
+    let get_open_bin_steps_response = OpenBinStepsResponse {
+        open_bin_steps: vec![20, 50, 100],
+    };
+
+    let get_all_lb_pairs_response = AllLBPairsResponse {
+        lb_pairs_available: vec![LBPairInformation::example(), LBPairInformation::example()],
+    };
+
+    // writeln!(file, "## Query Messages\n")?;
+    // print_query_messages!(
+    //     file,
+    //     get_min_bin_step,
+    //     get_fee_recipient,
+    //     get_lb_pair_implementation,
+    //     get_lb_token_implementation,
+    //     get_number_of_lb_pairs,
+    //     get_lb_pair_at_index,
+    //     get_number_of_quote_assets,
+    //     get_quote_asset_at_index,
+    //     is_quote_asset,
+    //     get_lb_pair_information,
+    //     get_preset,
+    //     get_all_bin_steps,
+    //     get_open_bin_steps,
+    //     get_all_lb_pairs
+    // );
+
+    writeln!(file, "## Query Messages with responses\n")?;
+    print_query_messages_with_responses!(
+        file,
+        (get_min_bin_step, get_min_bin_step_response),
+        (get_fee_recipient, get_fee_recipient_response),
+        (
+            get_lb_pair_implementation,
+            get_lb_pair_implementation_response
+        ),
+        (
+            get_lb_token_implementation,
+            get_lb_token_implementation_response
+        ),
+        (get_number_of_lb_pairs, get_number_of_lb_pairs_response),
+        (get_lb_pair_at_index, get_lb_pair_at_index_response),
+        (
+            get_number_of_quote_assets,
+            get_number_of_quote_assets_response
+        ),
+        (get_quote_asset_at_index, get_quote_asset_at_index_response),
+        (is_quote_asset, is_quote_asset_response),
+        (get_lb_pair_information, get_lb_pair_information_response),
+        (get_preset, get_preset_response),
+        (get_all_bin_steps, get_all_bin_steps_response),
+        (get_open_bin_steps, get_open_bin_steps_response),
+        (get_all_lb_pairs, get_all_lb_pairs_response),
+    );
+
+    Ok(())
+}
